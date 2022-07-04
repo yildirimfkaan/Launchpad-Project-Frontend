43a66c9195e9b9fd603de56728f953acda61d7e7
Melih Birim <11569268-melihbup@users.noreply.gitlab.com>
2022-07-04 11:19:03 +0000

    Deleted interface/components/Card1.js, interface/components/Footer.js, interface/components/Layout.js, interface/components/Nav.js, interface/pages/api/api_data.js, interface/pages/api/hello.js, interface/pages/project/[pid].js, interface/pages/404.js, interface/pages/_app.js, interface/pages/about.js, interface/pages/api.js, interface/pages/index.js, interface/public/favicon.ico, interface/public/ibss.ico, interface/public/vercel.svg, interface/styles/Footer.module.css, interface/styles/Home.module.css, interface/styles/Layout.module.css, interface/styles/Nav.module.css, interface/styles/globals.css, interface/.eslintrc.json, interface/next.config.js, interface/note.txt, interface/package-lock.json, interface/package.json

--- interface/pages/404.js
+++
@@ -1,9 +0,0 @@
-export default function Error() {
-    return (
-        <div>
-            <h1>
-                Böyle bir url mevcut değil
-            </h1>
-        </div>
-    );
-};
--- interface/pages/api/api_data.js
+++
@@ -1,9 +0,0 @@
-//export const defaultEndpoint = 'https://avalaunch-kyc.herokuapp.com/api/v1/projects';
-
-
-export async function api_json(){
-    var res = await fetch("https://avalaunch-kyc.herokuapp.com/api/v1/projects").then(res => res.json() );
-    return res['projects'];
-     
-  };
-
