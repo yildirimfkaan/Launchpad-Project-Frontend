b3c7ed8d81332b664349e4bbaeeeb5f0861e0bfa
Yunus Emre <y.emreturgut10@gmail.com>
2022-10-29 12:54:04 +0300

    footer done

--- unopad/src/App.js
+++ unopad/src/App.js
@@ -44,7 +44,7 @@ function App({ ...props }) {
         <Route
           path="*"
           render={(props) => (
-            <PublicLayout>
+            <PublicLayout {...props}>
               <PageNotFound />
             </PublicLayout>
           )}
