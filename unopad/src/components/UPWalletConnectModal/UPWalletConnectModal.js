6ab8114eb493f9770152ea98e1ba66043d160087
Baris Ipek <baris.ipek@unopad.com>
2022-11-01 07:24:35 +0000

    connect-wallet

--- unopad/src/components/UPWalletConnectModal/UPWalletConnectModal.js
+++ unopad/src/components/UPWalletConnectModal/UPWalletConnectModal.js
@@ -1,14 +1,16 @@
 /* eslint-disable max-len */
 
 import React from 'react';
-import Button from 'react-bootstrap/Button';
-import { Nav, Row, Col, Figure } from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
 
 import Modal from 'react-bootstrap/Modal';
 import { connect } from 'react-redux';
 import wallet from '../../helpers/wallet';
 import { WalletConnectModalAction } from '../../store/wallet/walletActions';
 import './UPWalletConnectModal.scss';
+import metamaskIcon from '../../assets/img/logo/metamask-icon.png';
+import coinbaseIcon from '../../assets/img/logo/coinbase.png';
+import trustwalletIcon from '../../assets/img/logo/trustwallet.png';
 
 function WalletConnectModal({ ...props }) {
   const { WalletConnectModal, WalletConnectModalRequest } = props;
@@ -20,41 +22,49 @@ function WalletConnectModal({ ...props }) {
   return (
     <>
       <Modal show={WalletConnectModal} onHide={handleClose} size="lg" centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Wallets</Modal.Title>
-        </Modal.Header>
+        <Modal.Header className="border-0" closeButton></Modal.Header>
         <Modal.Body>
-          <Row>
-            <Col>
-              <Figure>
-                <Nav.Link onClick={() => wallet.connectWallet()}>
-                  <Figure.Image
-                    width={150}
-                    height={150}
-                    src="https://1000logos.net/wp-content/uploads/2022/03/Coinbase-logo.png"
-                  />
-                </Nav.Link>
-              </Figure>
-            </Col>
-            <Col>
-              <Figure>
-                <Nav.Link onClick={() => wallet.connectWallet()}>
-                  <Figure.Image
-                    width={150}
-                    height={150}
-                    src="https://1000logos.net/wp-content/uploads/2022/05/MetaMask-Emblem.png"
-                  />
-                </Nav.Link>
-              </Figure>
-            </Col>
-          </Row>
-        </Modal.Body>
+          <div className="account-div text-fs-head-sm mb-4 ms-4 text-t-head-color">
+            Connect Wallet{' '}
+          </div>
+          <Nav.Link onClick={() => wallet.connectWallet()}>
+            <div className="d-flex modal-row mx-4">
+              <div className="d-flex align-items-center p-2">
+                <img className="wallet-icon ms-2" alt="metamask-icon" src={metamaskIcon} />
+                <span className="ms-4 text-fs-head-sm text-t-head-color">Metamask</span>
+              </div>
+            </div>
+          </Nav.Link>
 
-        <Modal.Footer className="justify-content-center">
-          <Button variant="danger" size="lg" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
+          <Nav.Link onClick={() => wallet.connectWallet()}>
+            <div className="d-flex modal-row align-items-center mx-4">
+              <div className="d-flex align-items-center">
+                <img className="wallet-icon ms-2" alt="coinbase-icon" src={coinbaseIcon} />
+                <span className="ms-4 text-fs-head-sm text-t-head-color">Coinbase</span>
+              </div>
+              <div className=" ms-auto p-2 new-logo px-3 me-1 d-flex align-items-center text-fs-body-sm">
+                New
+              </div>
+            </div>
+          </Nav.Link>
+          {/* <Nav.Link onClick={() => wallet.connectWallet()}> */}
+            <div className="d-flex modal-row align-items-center mx-4">
+              <div className="d-flex align-items-center">
+                <img className="wallet-icon ms-2" alt="trustwallet-icon" src={trustwalletIcon} />
+                <span className="ms-4 text-fs-head-sm text-t-head-color">Trust Wallet</span>
+              </div>
+              <div className=" ms-auto p-2 coming-soon-logo px-3 me-1 d-flex align-items-center text-fs-body-sm">
+                Coming Soon
+              </div>
+            </div>
+          {/* </Nav.Link> */}
+        </Modal.Body>
+        {/* <div className=" ms-auto p-2 new-logo px-3 me-1 d-flex align-items-center text-fs-body-sm ">
+          New
+        </div>
+        <div className=" ms-auto p-2 coming-soon-logo px-3 me-1 d-flex align-items-center text-fs-body-sm">
+          Coming Soon
+        </div> */}
       </Modal>
     </>
   );
