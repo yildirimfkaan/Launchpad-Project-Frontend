5a947bfc87ef780252a8e3598d067afbc22bff24
KaanYildirim01 <kaan.yildirim@ibss.com.tr>
2022-11-04 17:11:00 +0300

    activation page done

--- unopad/src/pages/Activation/Activation.js
+++ unopad/src/pages/Activation/Activation.js
@@ -1,9 +1,11 @@
+/* eslint-disable max-len */
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import UPFormControl from '../../components/UPFormControl/UPFormControl';
 import { activationRequest } from '../../store/account/userActions';
 import './Activation.scss';
+import { NavLink } from 'react-router-dom';
 
 const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
@@ -70,27 +72,45 @@ function Activation({ ...props }) {
   const { data, errors } = state;
   return (
     <>
-      <Row>
-        <Col>
-          <Form onSubmit={handleSubmit} style={{ textAlign: 'center' }}>
+    <Row className="d-flex justify-content-center align-items-center">
+      <Col className="public-activation-layout-col d-flex justify-content-center align-items-center bg-white px-1 py-2">
+        <div className="public-activation-layout-image d-md-flex d-none flex-column justify-content-center align-items-center bg-tertiary px-1 py-2">
+          <div className="text-fs-head-lg text-center text-light px-2">
+            Welcome to the future of <div className="text-primary">fundraising</div>
+          </div>
+          <div className="text-fs-body-md text-center text-light px-2">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua
+          </div>
+        </div>
+        <div className="d-flex flex-column justify-content-center aling-items-center px-4 mx-2 public-activation-layout-form">
+          <Form onSubmit={handleSubmit}>
             <UPFormControl
-              label="ActivationCode"
+              label="Activation Code"
               type="password"
               value={data.activationCode}
               handleChange={handleChange}
               error={errors.activationCode}
             />
-
             <Button
+              className="mb-4 mt-4 bg-unopad-primary col-sm-12"
               type="submit"
-              style={{ backgroundColor: '#365ae1', marginTop: '10px', justifyContent: 'center' }}
             >
-              Activate your Account
+              Activate your account
             </Button>
           </Form>
-          {/* <Button color="primary" onClick={this.handleSign}>SignUp</Button> */}
-        </Col>
-      </Row>
+          <Col className="m-1 d-flex justify-content-center">
+            <NavLink className="text-fs-body-md text-t-body-color" to="/signup">
+              Don't have an account yet?
+            </NavLink>
+            &nbsp; 
+            <NavLink className="signup-link text-primary" to="/signup">
+              SignUp
+            </NavLink>
+          </Col>
+        </div>
+      </Col>
+    </Row>
     </>
   );
 }
